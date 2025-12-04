dce44d7037b7ce0aa18a32ae2c0577c88823067a
nauar123 <nauarbahaa@gmail.com>
2025-12-04 14:52:27 +0100

    oprettet
    oprettet

---
+++ src/main/java/com/example/boernetoej/BoernetoejApplication.java
@@ -0,0 +1,13 @@
+package com.example.boernetoej;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoernetoejApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoernetoejApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/boernetoej/BoernetoejApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.boernetoej;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoernetoejApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
