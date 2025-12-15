7ea6fd74cfd372d0c7fc9c3a95595a4704b9cd3d
nauar123 <nauarbahaa@gmail.com>
2025-12-15 20:17:36 +0100

    fikset de sidste ting

--- src/main/java/com/example/boernetoej/BoernetoejApplication.java
+++ src/main/java/com/example/boernetoej/BoernetoejApplication.java
@@ -1,10 +1,9 @@
 package com.example.boernetoej;
 
-import jakarta.annotation.PostConstruct;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.crypto.password.PasswordEncoder;
+
 
 @SpringBootApplication
 public class BoernetoejApplication {
